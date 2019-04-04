d2068dc0d279c4741e3709922c548bf55b44fdd1
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:54:07 +0000

    Thu Apr 04 15:54:07 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,17 +6,18 @@ class FizzBuzz
   end
 
   def buzz?(number)
-    number.to_s.include?('3') || (number % 3).zero?
+    number.to_s.include?('5') || (number % 5).zero?
   end
 
   def fizz_buzz(number)
+    return 'fizz buzz' if fizz?(number) && buzz?(number)
     return 'fizz' if fizz?(number)
-    return 'buzz' if buzz?(number)
 
-    'fizz buzz' if fizz?(number) && buzz?(number)
+    'buzz' if buzz?(number)
   end
 
 end
+
 
 
 
