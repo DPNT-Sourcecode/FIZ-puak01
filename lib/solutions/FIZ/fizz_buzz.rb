4888018e548ceb761e55c67356c97f0f65d7acb5
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:46:45 +0000

    Thu Apr 04 15:46:45 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,11 +2,8 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    return 'fizz buzz' if (number % 3).zero? && (number % 5).zero?
-    return 'fizz' if (number % 3).zero?
-    return 'buzz' if (number % 5).zero?
-
-    number
+    'fizz'
   end
 
 end
+
