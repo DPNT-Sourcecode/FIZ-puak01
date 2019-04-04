252b2f7d7a16b1cdb72c82f3b530c0de73f154a9
sbx_user1059 <sbx_user1059@ip-10-128-166-47.eu-west-2.compute.internal>
2019-04-04 15:30:57 +0000

    Thu Apr 04 15:30:57 UTC 2019

--- lib/solutions/HLO/hello.rb
+++ lib/solutions/HLO/hello.rb
@@ -2,7 +2,7 @@
 class Hello
 
   def hello(friend_name)
-    raise 'Not implemented'
+    "Hello, #{friend_name}"
   end
 
 end
---
+++ test/HLO/hello_test.rb
@@ -0,0 +1,15 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_hello
+    assert_equal 'Hello, Mike', Hello.new.hello('Mike'), 'App should greet the world'
+  end
+
+end
